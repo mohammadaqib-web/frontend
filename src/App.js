41393d8b2e58f0ce6dbe4a37c0ab845f42b13af8
Mohammad Aqib <88379400+mohdaqib234@users.noreply.github.com>
2024-07-21 09:05:58 +0530

    frontend not responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './assets/Mentoons logo.png';
+import inventor from './assets/Camera/Camera.png';
+import Camera from './assets/Camera/Camera 2.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header text-center p-2 shadow-lg">
+        <img src={Logo} width="200px" alt="logo"/>
+      </div>
+      <div className="quiz-section m-4 d-flex justify-content-center align-items-center">
+        <div className="row g-0 quiz-row rounded">
+          <div className="col d-flex align-items-center justify-content-center">
+            <img src={inventor} alt="Inventor Image" height="350px" />
+          </div>
+          <div className="col p-2 text-center">
+            <form>
+            <h3 className="text-center">Who Invented Camera?</h3>
+            <img src={Camera} alt="camera" width="100px"/>
+            <div className="select-inventor rounded mt-4 p-2">
+              <input type="radio" name="inventor" value="Dennis" />
+              <label className="form-label ms-3">Nicéphore</label>
+            </div>
+            <div className="select-inventor rounded mt-4 p-2">
+              <input type="radio" name="inventor" value="Mark" />
+              <label className="form-label ms-3">Mark</label>
+            </div>
+            <div className="select-inventor rounded mt-4 p-2">
+              <input type="radio" name="inventor" value="Steve" />
+              <label className="form-label ms-3">Steve</label>
+            </div>
+            <div className="select-inventor rounded mt-4 p-2">
+              <input type="radio" name="inventor" value="John" />
+              <label className="form-label ms-3">John</label>
+            </div>
+            <div className="d-flex justify-content-center mt-3">
+            <button className="btn btn-success" type="submit">Submit</button>
+            </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
